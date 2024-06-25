c2754ba8b91b255c869545cc645b3e595cf0d929
Devin Davies <devin.m.davies@gmail.com>
2024-06-25 16:55:25 -0400

    add tracking scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<App />
+		<Analytics />
+		<SpeedInsights />
 	</React.StrictMode>,
 );
