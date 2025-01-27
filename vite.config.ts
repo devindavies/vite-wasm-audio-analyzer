c6d0179e620133ec3aa1af1f4ef58188d9c87fac
Devin Davies <devin.m.davies@gmail.com>
2025-01-27 23:49:21 +0000

    upgrade tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
+		tailwindcss(),
 		viteStaticCopy({
 			targets: [
 				{
